cf2af6a3d8c4eb9eec9eded9a1bd690e53930924
tar2ga <tar2ga@ya.ru>
2021-03-02 16:16:11 +0300

    mini fix

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	const burger = document.querySelector('.header__burger');
 
 	burger.onclick = () => {
-		document.querySelector('.header__burger').classList.toggle('active');
+		burger.classList.toggle('active');
 		document.querySelector('.header__menu').classList.toggle('active');
 		document.querySelector('body').classList.toggle('lock');
 	}
